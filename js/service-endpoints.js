b8161bc762417194e8ca68e83c413cce0c7067b1
cyc0207 <152088528+cyc0207@users.noreply.github.com>
2026-07-17 15:34:58 +0800

    chore: point services to current database tunnel

---
+++ js/service-endpoints.js
@@ -0,0 +1,5 @@
+// Public service locations only. Keep API keys and other credentials out of this file.
+window.DECORATE_ME_CONFIG = Object.assign(window.DECORATE_ME_CONFIG || {}, {
+    memberDatabaseUrl: 'https://fact-mice-prince-voltage.trycloudflare.com',
+    productUrl: 'https://fact-mice-prince-voltage.trycloudflare.com'
+});
